ae665928592a1d7f666ab0712d40f68fd4302576
Cristian Escudero <escudero89@gmail.com>
2011-10-16 00:01:12 -0300

    python wallpaper changer

---
+++ python/wallpaper_changer/wallpaper_changer.py
@@ -0,0 +1,73 @@
+import os, fnmatch
+
+from xml.dom.minidom import Document
+
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+def createXml(subdirectory='', dura_f='1795.0', dura_t='5.0'):
+	atributos = ["year","month","day","hour","minute","second"]
+	atrdentros = ["2009","08","04","00","00","00"]
+	
+	directorios = showFiles(subdirectory)
+	
+	string = "<background>\n" # Voy creando basicamente con string
+	
+	# Principio basico
+	string += "  <starttime>\n"
+	for atributo, atrdentro in zip(atributos,atrdentros):
+		string += "    <" + atributo + ">" + atrdentro + "</" + atributo + ">\n"
+	string += "  </starttime>\n"
+	
+	# Comentario del mismo archivo
+	string += "<!-- This animation will start at midnight. -->\n" 
+	
+	for i in range(len(directorios)):
+		# Documentos creados estaticos
+		string += "  <static>\n"
+		string += "    <duration>" + dura_f + "</duration>\n"
+		string += "    <file>" + directorios[i] + "</file>\n"
+		string += "  </static>\n"
+		# Documentos creados dinamicos
+		string += "  <transition>\n"
+		string += "    <duration>" + dura_t + "</duration>\n"
+		string += "    <from>" + directorios[i] + "</from>\n"
+		if i + 1 == len(directorios):  # Circular
+			proximo = 0
+		else:
+			proximo = i + 1
+		string += "    <to>" + directorios[proximo] + "</to>\n"
+		string += "  </transition>\n"
+
+	string += "</background>"
+
+	return string
+
+
+def showFiles(subdirectory=''):	
+    if subdirectory:
+        path = subdirectory
+    else:
+        path = os.getcwd()
+        
+    directorios = []
+    for root, dirs, names in os.walk(path):
+		for name in names:
+			directorios.append(root + "/" + name)
+		return directorios
+	
+if __name__ == "__main__":
+    import sys
+    
+    files = open("background-1.xml","w")
+        
+    l = len(sys.argv)
+    string = createXml()
+    
+    if l > 3:
+	    string = createXml(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif l > 2:
+	    string = createXml(sys.argv[1], sys.argv[2])
+    elif l > 1:
+		string = createXml(sys.argv[1])    
+    
+    print >> files, string
