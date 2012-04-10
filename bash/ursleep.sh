68d6d5f1058493785e4c3acd8dfa86a9d016987a
Cristian Escudero <escudero89@gmail.com>
2012-04-10 13:36:16 -0300

    Added feature

--- bash/ursleep.sh
+++ bash/ursleep.sh
@@ -14,41 +14,63 @@
 # You should have received a copy of the GNU General Public License along 
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-separador="#------------------------------------------------------------------";
+c[0]="\033[1;32m"; # verde
+c[1]="\e[1;31m"; # rojo
+c[2]="\e[1;34m"; # celeste
+c[3]="\e[1;33m"; # amarillo
+c[4]="\e[1;30m"; # gris
+c[5]="\033[0m";
 
-color[0]="\033[1;32m"; # verde
-color[1]="\e[1;31m"; # rojo
-color[2]="\e[1;34m"; # celeste
-color[3]="\e[1;33m"; # amarillo
-color[4]="\e[1;30m"; # gris
-color[5]="\033[0m";
+function despertarse {
+
+    echo -e -n "# Hora actual:" ${c[1]}$(date +%H:%M)${c[5]}" + (minutos [0]) ";
+    read extra
+    
+    if [[ $extra =~ ^[0-9]+$ ]]; then
+        tiempo_modificado=$(date --date "$dte $extra minutes" +"%H:%M");
+        echo -e "# Hora modificada:" ${c[3]}$tiempo_modificado${c[5]}"."
+    else   
+        extra=0;
+    fi
+    
+    tiempo_dormir=$(date --date "$dte $((45+$extra)) minutes" +"%H:%M");
+
+    echo -e "# Probablemente estes durmiendo a las:" ${c[0]}$tiempo_dormir${c[5]}".";
+
+    mins=$((105+$extra));
+    step=90;
+
+    for i in $(seq 1 6); do
+        echos[$i]=$(date --date "$dte $mins minutes" +"%H:%M");
+        
+        mins=$(($mins+$step));
+    done
+
+    echosjoin=$(printf ${c[2]}" %s "${c[5]} "${echos[@]}")
+
+    echo "#" 
+    echo "# Prueba levantarte en los siguientes horarios:"
+    echo -e "#" $echosjoin
+
+} 
+
+separador="#------------------------------------------------------------------";
 
 echo $separador;
 
-echo -e "# ${color[3]}URSLEEP, a bedtime calculator!${color[5]} (Basado en http://sleepyti.me/)"
-echo -e "# ${color[4]}# adaptado por Cristian Escudero (escudero89@gmail.com).${color[5]}"
-echo -e "# ${color[4]}Este archivo se encuentra bajo licencia GPLv3.${color[5]}";
+echo -e "# ${c[3]}URSLEEP, a bedtime calculator!${c[5]} (Basado en http://sleepyti.me/)"
+echo -e "# ${c[4]}adaptado por Cristian Escudero (escudero89@gmail.com).${c[5]}"
+echo -e "# ${c[4]}Este archivo se encuentra bajo licencia GPLv3.${c[5]}";
 echo "#"
 
-echo -e "# Hora actual:" ${color[1]}$(date +%H:%M)${color[5]}".";
+echo -n "# Ingrese una opcion ([I]rse a dormir, [d]espertarse): " 
+read opcion
 
-tiempo_dormir=$(date --date "$dte 45 minutes" +"%H:%M");
-
-echo -e "# Probablemente estes durmiendo a las:" ${color[0]}$tiempo_dormir${color[5]}".";
-
-mins=105;
-step=90;
-
-for i in $(seq 1 6); do
-    echos[$i]=$(date --date "$dte $mins minutes" +"%H:%M");
+if [[ $opcion == "d" ]]; then
+    echo "# Not implemented yet."
     
-    mins=$(($mins+$step));
-done
-
-echosjoin=$(printf ${color[2]}" %s "${color[5]} "${echos[@]}")
-
-echo "#" 
-echo "# Prueba levantarte en los siguientes horarios:"
-echo -e "#" $echosjoin
+else
+    despertarse;
+fi
 
 echo $separador; 
