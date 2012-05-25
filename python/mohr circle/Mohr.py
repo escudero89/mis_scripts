509c4240d3728e7c6823758ada4edbb3a33111bc
Cristian Escudero <escudero89@gmail.com>
2012-05-25 20:30:41 -0300

    Added license and readme.

--- python/mohr circle/Mohr.py
+++ python/mohr circle/Mohr.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+# <MOHR: A tool for draw Mohr's Circles.>
+#
+# Copyright (C) 2012 Cristian Gabriel Escudero escudero89@gmail.com
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Modo de uso:
+# Desde consola llamar: python Mohr "a" "b" "c"
+# Donde a = sigma_x, b = sigma_y, c = sigma_xy = sigma_yx
+# Siendo estas las componentes del tensor de tensiones.
 
 import math, sys
 import numpy as np
